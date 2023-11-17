7be1eb0b126b55e50e6de221f9f0142d11035735
AquaEL007 <gvgenov@gmail.com>
2023-11-17 13:10:40 +0200

    Add project files.

---
+++ 03.ExtractFile/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _03.ExtractFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Console.ReadLine();
+
+            string fileName = string.Empty;
+            string fileExtension = string.Empty;
+
+            int lastSeparatorIndex = filePath.LastIndexOf('\\');
+            int extentionIndex = filePath.LastIndexOf('.');
+
+            if (lastSeparatorIndex != -1 && extentionIndex != -1 && lastSeparatorIndex < extentionIndex )
+            {
+                fileName = filePath.Substring(lastSeparatorIndex + 1, extentionIndex - lastSeparatorIndex - 1);
+                fileExtension = filePath.Substring(extentionIndex + 1);
+            }
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtension}");
+
+        }
+    }
+}
---
+++ 05.MultiplyBigNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿
+namespace _05.MultiplyBigNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bigNumber = Console.ReadLine();
+            string multiplayNumber = Console.ReadLine();
+
+            Console.WriteLine(MultiplayNumbers(bigNumber, multiplayNumber));
+        }
+
+        private static string MultiplayNumbers(string bigNumber, string multiplayNumber)
+        {
+            if (bigNumber == "0" || multiplayNumber == "0")
+            {
+                return "0";
+            }
+
+            int carry = 0;
+            int multiplayer = int.Parse(multiplayNumber);
+
+            char[] resultChars = new char[bigNumber.Length + 1];
+
+            for (int i = bigNumber.Length - 1; i >= 0; i--)
+            {
+                int digit = int.Parse(bigNumber[i].ToString());
+                int product = digit * multiplayer + carry;
+
+                resultChars[i + 1] = (char)(product % 10 + '0');
+                carry = product / 10;
+            }
+
+            if (carry > 0)
+            {
+                resultChars[0] = (char)(carry + '0');
+            }
+
+            return new string(resultChars).TrimStart('\0');
+        }
+    }
+}
